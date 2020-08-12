c7e2ea2f16f1cf49374f0bfa03b200b3b542d3d2
goncmodev <gon.cmo.dev@gmail.com>
2020-08-13 00:08:44 +0200

    Cargar un componente y otro

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 
   const [ presupuesto, guardarPresupuesto ] = useState(0);
   const [ restante, guardarRestante ] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
 
 
   return (
@@ -18,22 +19,29 @@ function App() {
         <h1>Gasto semanal</h1>
 
         <div className='contenido-principal contenido'>
-          <Pregunta 
-          
-          guardarPresupuesto={guardarPresupuesto}
-          guardarRestante={guardarRestante}
-          />
-
-          <div className='row'>
-            <div className='one-half column'>
-              <Formulario />
-            </div>
+          { mostrarpregunta ? 
+            ( 
+              <Pregunta 
+                guardarPresupuesto={guardarPresupuesto}
+                guardarRestante={guardarRestante}
+                actualizarPregunta={actualizarPregunta}
+              />
+            ) :
             
-            <div className='one-half column'>
-              2
-            </div>
-          </div>
-
+            (
+              <div className='row'>
+                <div className='one-half column'>
+                  <Formulario />
+                </div>
+              
+                <div className='one-half column'>
+                  2
+                </div>
+              </div>
+            )
+            
+          }
+        
         </div>
       </header>
     </div>
